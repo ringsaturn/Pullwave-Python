0b092219f5eb22674aa5d70f829d8d5a5a26b884
ringsaturn <tltrc919@gmail.com>
2017-05-20 20:46:45 +0800

    修改了文件中的小问题

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 * matplotlib
 
 ## 用法
-直接运行程序，会提示输入 词语，然后会绘制柱状图。会提示是否使用微博高级搜索功能（代码复用自 [Devonthink-Chinese-Seach](https://github.com/ringsaturn/DEVONthink-Chinese-Search) 项目）
+Pro：直接运行程序，会提示输入 词语，然后会绘制柱状图。会提示是否使用微博高级搜索功能（代码复用自 [Devonthink-Chinese-Seach](https://github.com/ringsaturn/DEVONthink-Chinese-Search) 项目）
+simple：只是单纯得绘制曲线
 
 目前 2017-05-15 还只能检索一个词，两个词的还需要等一阵子，并且还比较糙，细节上还有很多要改进的。
 
@@ -20,12 +21,10 @@
 - [ ] 增加保存图片的功能
 - [ ] 争取做个比官网更强大的 GUI 程序出来
 - [ ] 增加检测峰值点
-- [x] 利用 Weibo 高级搜索检索尝试检索相关新闻事件。2017-05-18 完成[^1]
+- [x] 利用 Weibo 高级搜索检索尝试检索相关新闻事件。2017-05-18 完成
 
 感谢 [Pullwave](http://www.pullwave.com) 提供的接口，让我可以直接得到感兴趣的词的趋势。
 
 ## 备注
 * 为了加快程序启动速度，部分模块一直到使用时才 `import`
-
-
-[^1]: 如用Pullwave检索`ios`时，在4月20日有一个峰值。在微博中高级搜索很容易得知是苹果让微信关闭文章打赏功能
+* 之所以增加搜索功能，是因为如用Pullwave检索`ios`时，在4月20日有一个峰值。在微博中高级搜索很容易得知是苹果让微信关闭文章打赏功能
--- pullwave-simple.py
+++ pullwave-simple.py
@@ -5,9 +5,9 @@ import time
 import json
 
 def pullwave(word):
-	date = time.strftime("%Y-%m-%d", time.localtime()) 
+	date = time.strftime("%Y-%m-%d", time.localtime())
 	url = 'http://www.pullwave.com/get.php?json=1&auth_usr=free_vip&w1='+str(word)+'&w2=&end_date='+str(date)
-	resp = requests.get(url) 
+	resp = requests.get(url)
 	data_json = resp.text
 	return data_json
 
@@ -16,32 +16,32 @@ def pplot(word):
 	data_dic = json.loads(data_raw)
 	d = data_dic
 	qushi = data_dic['qushi']
-	
+
 	x= len(qushi)*['']
 	y = len(qushi)*['']
-	
+
 	for i, v in enumerate(qushi):
 		x[i] = v['date']
 		y[i] = v['v']
-		
+
 	from datetime import datetime
 	x = [datetime.strptime(i, '%Y-%m-%d') for i in x]
 	y = [float(i) for i in y]
-	
-	#import matplotlib 
-	#matplotlib.use('Agg')  
-	
+
+	#import matplotlib
+	#matplotlib.use('Agg')
+
 	import matplotlib.pyplot as plt
-	
+
 	plt.rcParams['font.sans-serif'] = ['SimHei']
-	
+
 	plt.bar(x, y)
 	plt.xlabel(u'年-月-日')
 	plt.ylabel(u'讨论量')
 	plt.title(u' %s 在社交网络上的讨论量趋势图' %word)
 	plt.show()
-	#plt.savefig('/Users/ringsaturn/Desktop/%s.png' %word,dpi = 720)  
+	#plt.savefig('/Users/[username]/Desktop/%s.png' %word,dpi = 720)  
 	return x, y
-	
+
 word = input('要查询的词汇： ')
 x, y = pplot(word)
